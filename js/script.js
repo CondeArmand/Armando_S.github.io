80f44835ffa72fbc8a8c333c1ae6ffeb08ebb407
CondeArmand <104652384+CondeArmand@users.noreply.github.com>
2023-03-20 13:07:32 -0300

    Corrigindo erros

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 let menu = document.querySelector('#menu-icon');
 let nav = document.querySelector('.navbar');
 
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('open');
 }
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('open');
+}
+
+
+
+
+
+
+
